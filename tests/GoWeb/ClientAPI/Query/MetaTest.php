05bc61df21f19cf4e593bdfbc4d58c5a7595bd5b
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-05-22 17:39:59 +0300

    fix meta test

--- tests/GoWeb/ClientAPI/Query/MetaTest.php
+++ tests/GoWeb/ClientAPI/Query/MetaTest.php
@@ -6,26 +6,28 @@ class MetaTest extends \Guzzle\Tests\GuzzleTestCase
 {
     public function testSend()
     {
+        $response = array(
+            "error" => 0,
+            "name" => "GoWeb",
+            "description" => array(
+                "uk" => "P2P television",
+                "en" => "P2P television",
+                "ru" => "P2P television",
+                "be" => "P2P television",
+            ),
+            "icon" => "http:\/\/tvapi.goweb.com\/img\/logo\/goweb.png",
+            "time" => 1400760359
+        );
+        
         $clientApi = new \GoWeb\ClientAPI('http://api.mw/1.0');
         $clientApi->addSubscriber(new \Guzzle\Plugin\Mock\MockPlugin(array(
             new \Guzzle\Http\Message\Response(200, array(
                 'Content-type' => 'application/json',
-            ), json_encode(array(
-                "error" => 0,
-                "name" => "GoWeb",
-                "description" => array(
-                    "uk" => "P2P television",
-                    "en" => "P2P television",
-                    "ru" => "P2P television",
-                    "be" => "P2P television",
-                ),
-                "icon" => "http:\/\/tvapi.goweb.com\/img\/logo\/goweb.png",
-                "time" => 1400760359
-            ))),
+            ), json_encode($response)),
         )));
         
         $meta = $clientApi->query('Meta')->send();
         
-        var_dump($meta->toArray());
+        $this->assertEquals($response, $meta->toArray());
     }
 }
