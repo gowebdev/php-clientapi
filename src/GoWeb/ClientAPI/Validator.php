58d3c692291a0bf3520b868070e6ba9861317098
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-05-23 22:44:46 +0300

    add validator

---
+++ src/GoWeb/ClientAPI/Validator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace GoWeb\ClientAPI;
+
+class Validator extends \Sokil\Rest\Client\Request
+{
+    public function isValid()
+    {
+        
+    }
+    
+    public function getReport()
+    {
+        
+    }
+}
