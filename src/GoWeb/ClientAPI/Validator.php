4941b8ee6ac8af8d85be0ad0be0bca48eec36f48
ltm <web.4life@yahoo.com>
2014-06-19 01:16:08 +0300

    vod feed validation

--- src/GoWeb/ClientAPI/Validator.php
+++ src/GoWeb/ClientAPI/Validator.php
@@ -4,9 +4,18 @@ namespace GoWeb\ClientAPI;
 
 class Validator
 {
-    const ERROR_TYPE_FIELD_EMPTY = 0;
-    const ERROR_TYPE_FIELD_MUSTBEARRAY = 0;
-    
+    const ERROR_TYPE_FIELD_REQUIRED = 0;
+    const ERROR_TYPE_FIELD_EMPTY = 1;
+    const ERROR_TYPE_FIELD_MUSTBESTRING = 2;
+    const ERROR_TYPE_FIELD_MUSTBEINT = 3;
+    const ERROR_TYPE_FIELD_MUSTBEBOOL = 4;
+    const ERROR_TYPE_FIELD_MUSTBEARRAY = 5;
+    const ERROR_TYPE_FIELD_MUSTBEFLOAT = 6;
+    const ERROR_TYPE_FIELD_OUTOFRANGE = 7;
+    const ERROR_TYPE_FIELD_OVERLENGTHLIMIT = 8;
+    const ERROR_TYPE_FIELD_WRONGDATEFORMAT = 9;
+    const ERROR_TYPE_FIELD_MUSTBETIMESTAMP = 10;
+
     /**
      *
      * @var \GoWeb\ClientAPI
@@ -113,9 +122,169 @@ class Validator
         
     }
     
-    private function _checkVodFeed()
+    protected function _checkVodFeed()
     {
-        
+        $url = '/vod/feed';
+
+        // response
+        $response = $this->_clientAPI
+            ->createRequest('Films')
+            ->getResponse()
+            ->toArray();
+
+        // validation errors
+
+        //total_items
+        if (!isset($response['total_items'])) {
+            $this->recordError($url, 'total_items', self::ERROR_TYPE_FIELD_REQUIRED);
+
+        } elseif ((string) (int) $response['total_items'] !== (string) $response['total_items']) {
+            $this->recordError($url, 'total_items', self::ERROR_TYPE_FIELD_MUSTBEINT);
+        }
+
+        //items
+        if (!isset($response['items'])) {
+            $this->recordError($url, 'items', self::ERROR_TYPE_FIELD_REQUIRED);
+
+        } elseif (!is_array($response['items'])) {
+            $this->recordError($url, 'items', self::ERROR_TYPE_FIELD_MUSTBEARRAY);
+
+        } else {
+            if (count($response['items']) < 1) {
+                $this->recordError($url, 'items.id', self::ERROR_TYPE_FIELD_REQUIRED);
+                $this->recordError($url, 'items.name', self::ERROR_TYPE_FIELD_REQUIRED);
+                $this->recordError($url, 'items.description', self::ERROR_TYPE_FIELD_REQUIRED);
+                $this->recordError($url, 'items.category', self::ERROR_TYPE_FIELD_REQUIRED);
+                $this->recordError($url, 'items.filesize', self::ERROR_TYPE_FIELD_REQUIRED);
+                $this->recordError($url, 'items.thumb', self::ERROR_TYPE_FIELD_REQUIRED);
+                $this->recordError($url, 'items.ad', self::ERROR_TYPE_FIELD_REQUIRED);
+            }
+
+            foreach ($response['items'] as $item) {
+
+                // items.id
+                if (!isset($item['id'])) {
+                    $this->recordError($url, 'items.id', self::ERROR_TYPE_FIELD_REQUIRED);
+
+                } elseif ((string) (int) $item['id'] !== (string) $item['id']) {
+                    $this->recordError($url, 'items.id', self::ERROR_TYPE_FIELD_MUSTBEINT);
+                }
+
+                // items.name
+                if (!isset($item['name'])) {
+                    $this->recordError($url, 'items.name', self::ERROR_TYPE_FIELD_REQUIRED);
+
+                } elseif (!is_string($item['name'])) {
+                    $this->recordError($url, 'items.name', self::ERROR_TYPE_FIELD_MUSTBESTRING);
+                }
+
+                // items.description
+                if (!isset($item['description'])) {
+                    $this->recordError($url, 'items.description', self::ERROR_TYPE_FIELD_REQUIRED);
+
+                } elseif (!is_string($item['description'])) {
+                    $this->recordError($url, 'items.description', self::ERROR_TYPE_FIELD_MUSTBESTRING);
+                }
+
+                // items.hd
+                if (isset($item['hd'])) {
+                    if ((string) (int) $item['hd'] !== (string) $item['hd']) {
+                        $this->recordError($url, 'items.hd', self::ERROR_TYPE_FIELD_MUSTBEINT);
+
+                    } elseif (!in_array((int)$item['hd'], [0, 1])) {
+                        $this->recordError($url, 'items.hd', self::ERROR_TYPE_FIELD_OUTOFRANGE);
+                    }
+                }
+
+                // items.year
+                if (isset($item['year']) && (string)(int) $item['year'] !== (string) $item['year']) {
+                    $this->recordError($url, 'items.year', self::ERROR_TYPE_FIELD_MUSTBEINT);
+                }
+
+                // items.rate
+                if (isset($item['rate'])) {
+                    if ((string) (int) $item['rate'] !== (string) $item['rate']) {
+                        $this->recordError($url, 'items.rate', self::ERROR_TYPE_FIELD_MUSTBEINT);
+
+                    } elseif (!in_array((int)$item['rate'], [1, 2, 3, 4, 5])) {
+                        $this->recordError($url, 'items.rate', self::ERROR_TYPE_FIELD_OUTOFRANGE);
+                    }
+                }
+
+                // items.category
+                if (!isset($item['category'])) {
+                    $this->recordError($url, 'items.category', self::ERROR_TYPE_FIELD_REQUIRED);
+
+                } elseif ((string) (int) $item['category'] !==  (string) $item['category']) {
+                    $this->recordError($url, 'items.category', self::ERROR_TYPE_FIELD_MUSTBEINT);
+                }
+
+                // items.genres
+                if (isset($item['genres']) && !is_array($item['genres'])) {
+                    $this->recordError($url, 'items.genres', self::ERROR_TYPE_FIELD_MUSTBEARRAY);
+                }
+
+                // items.director
+                if (isset($item['director']) && !is_string($item['director'])) {
+                    $this->recordError($url, 'items.director', self::ERROR_TYPE_FIELD_MUSTBESTRING);
+                }
+
+                // items.actors
+                if (isset($item['actors']) && !is_string($item['actors'])) {
+                    $this->recordError($url, 'items.actors', self::ERROR_TYPE_FIELD_MUSTBESTRING);
+                }
+
+                // items.country
+                if (isset($item['country']) && !is_string($item['country'])) {
+                    $this->recordError($url, 'items.country', self::ERROR_TYPE_FIELD_MUSTBESTRING);
+                }
+
+                // items.duration
+                if (isset($item['duration']) && (string)(int) $item['duration'] !== (string) $item['duration']) {
+                    $this->recordError($url, 'items.duration', self::ERROR_TYPE_FIELD_MUSTBEINT);
+                }
+
+                // items.url
+                if (isset($item['url']) && !is_string($item['url'])) {
+                    $this->recordError($url, 'items.url', self::ERROR_TYPE_FIELD_MUSTBESTRING);
+                }
+
+                // items.torrent
+                if (isset($item['torrent']) && !is_string($item['torrent'])) {
+                    $this->recordError($url, 'items.torrent', self::ERROR_TYPE_FIELD_MUSTBESTRING);
+                }
+
+                // items.hlts1
+                if (isset($item['hlts1']) && !is_string($item['hlts1'])) {
+                    $this->recordError($url, 'items.hlts1', self::ERROR_TYPE_FIELD_MUSTBESTRING);
+                }
+
+                // items.filesize
+                if (!isset($item['filesize'])) {
+                    $this->recordError($url, 'items.filesize', self::ERROR_TYPE_FIELD_REQUIRED);
+
+                } elseif ((string)(int) $item['filesize'] !== (string) $item['filesize']) {
+                    $this->recordError($url, 'items.filesize', self::ERROR_TYPE_FIELD_MUSTBEINT);
+                }
+
+                // items.thumb
+                if (!isset($item['thumb'])) {
+                    $this->recordError($url, 'items.thumb', self::ERROR_TYPE_FIELD_REQUIRED);
+
+                } elseif (!is_string($item['thumb'])) {
+                    $this->recordError($url, 'items.thumb', self::ERROR_TYPE_FIELD_MUSTBESTRING);
+                }
+
+                // items.ad
+                if (!isset($item['ad'])) {
+                    $this->recordError($url, 'items.ad', self::ERROR_TYPE_FIELD_REQUIRED);
+
+                } elseif (!is_string($item['ad']) && !is_array($item['ad'])) {
+                    $this->recordError($url, 'items.ad', self::ERROR_TYPE_FIELD_MUSTBESTRING);
+                    $this->recordError($url, 'items.ad', self::ERROR_TYPE_FIELD_MUSTBEARRAY);
+                }
+            }
+        }
     }
     
     private function _checkVodCategories()
--- tests/GoWeb/ClientAPI/ValidatorTest.php
+++ tests/GoWeb/ClientAPI/ValidatorTest.php
@@ -2,9 +2,65 @@
 
 namespace GoWeb\ClientAPI;
 
-class ValidatorTest extends \Guzzle\Tests\GuzzleTestCase 
+class ValidatorWrapper extends Validator
+{
+    public function checkVodFeed() {
+        $this->_checkVodFeed();
+    }
+}
+
+class ValidatorTest extends \Guzzle\Tests\GuzzleTestCase
 {
     protected $_clientAPI;
+
+    protected $_demoUser;
+
+    public function setUp()
+    {
+        // configure client api
+        $this->_clientAPI = new \GoWeb\ClientAPI(array(
+            'apiServerUrl'  => 'http://dkjglkdsfjgkldsfjgkldsfjglkdsfjglkdfsgjldksfjgkldfg.com/1.0/',
+            'cacheAdapter' => new CacheAdapterMock,
+        ));
+
+        $this->_demoUser =  array(
+            "error" => 0,
+            "token" => "5440644e27a3259da3feaab416f37cee06d12569",
+            "status" => 0,
+            "balance" => array(
+                "amount" => 0,
+                "currency" => "EUR"
+            ),
+            "profile" => array(
+                "id" => 0,
+                "first_name" => "Гість",
+                "last_name" => "",
+                "contract_number" => 0,
+                "status" => "ACTIVE"
+            ),
+            "baseServices" => [
+                array(
+                    "id" => 0,
+                    "service_id" => 6,
+                    "name" => "Рекламний",
+                    "cost" => 0,
+                    "catchup" => 0,
+                    "ad" => 1,
+                    "total_cost" => 0
+                )
+            ],
+            "speed" => array(
+                "120KB" => "120 килобит в секунду",
+                "512KB" => "512 килобит в секунду",
+                "1MB" => "1 мегабит в секунду",
+                "2MB" => "2 мегабита в секунду",
+                "8MB" => "8 мегабит в секунду",
+                "36MB" => "36 мегабит в секунду"
+            ),
+            "time" => 1402652985
+        );
+    }
+
 
     /**
      * @expectedException \GoWeb\ClientAPI\Validator\Exception\UnknownHost
@@ -19,5 +75,190 @@ class ValidatorTest extends \Guzzle\Tests\GuzzleTestCase
         
         $validator = $this->_clientAPI->getValidator();
         $validator->isValid();
+    }
+
+    public function testVodFeedValid()
+    {
+        // mock response
+        $this->_clientAPI->addSubscriber(new \Guzzle\Plugin\Mock\MockPlugin(array(
+            // auth
+            new \Guzzle\Http\Message\Response(200, array(
+                'Content-type' => 'application/json',
+            ), json_encode($this->_demoUser)),
+
+            // vod feed
+            new \Guzzle\Http\Message\Response(200, array(
+                'Content-type' => 'application/json',
+            ), json_encode([
+                "error" => 0,
+                "total_items" => 200,
+                "items" => [
+                    [
+                        "id" => 1,
+                        "name" => "Из Парижа с любовью",
+                        "description" => "Примерного сотрудника американского посольства...",
+                        "hd" => 1,
+                        "year" => 2010,
+                        "category" => 1,
+                        "director" => "Пьер Морел",
+                        "actors" => "Джон Траволта, Джонатан Рис-Майерс, Касия Смутняк...",
+                        "duration" => 92,
+                        "url" => "http://ivf.goweb.com/apt/ba.mp4?sign=yQqhDoGyXybw-hIdPgtchg&exp=1427454471",
+                        "torrent:" => "http://server.com/path-to.torrent",
+                        "filesize" => 6584893674,
+                        "thumb" => "http://iptv.dev.telehouse-ua.net/screenshots/000/003/380.jpg",
+                        "ad" => []
+                    ]
+                ]
+            ])),
+        )));
+
+        $validator = new ValidatorWrapper($this->_clientAPI);
+        $validator->checkVodFeed();
+        $this->assertNull($validator->getReport()['/vod/feed']);
+    }
+
+    public function testVodFeedRequiredFields()
+    {
+        // mock response
+        $this->_clientAPI->addSubscriber(new \Guzzle\Plugin\Mock\MockPlugin(array(
+            // auth
+            new \Guzzle\Http\Message\Response(200, array(
+                'Content-type' => 'application/json',
+            ), json_encode($this->_demoUser)),
+
+            // empty response
+            new \Guzzle\Http\Message\Response(200, array(
+                'Content-type' => 'application/json',
+            ), json_encode([
+                "error" => 0,
+            ])),
+
+            // empty items
+            new \Guzzle\Http\Message\Response(200, array(
+                'Content-type' => 'application/json',
+            ), json_encode([
+                "error" => 0,
+                "total_items" => 200,
+                "items" => []
+            ])),
+        )));
+
+        $validator = new ValidatorWrapper($this->_clientAPI);
+
+        // empty response
+        $validator->checkVodFeed();
+
+        // empty items
+        $validator->checkVodFeed();
+
+        $this->assertEquals([
+            "total_items"       => [$validator::ERROR_TYPE_FIELD_REQUIRED],
+            "items"             => [$validator::ERROR_TYPE_FIELD_REQUIRED],
+            "items.id"          => [$validator::ERROR_TYPE_FIELD_REQUIRED],
+            "items.name"        => [$validator::ERROR_TYPE_FIELD_REQUIRED],
+            "items.description" => [$validator::ERROR_TYPE_FIELD_REQUIRED],
+            "items.category"    => [$validator::ERROR_TYPE_FIELD_REQUIRED],
+            "items.filesize"    => [$validator::ERROR_TYPE_FIELD_REQUIRED],
+            "items.thumb"       => [$validator::ERROR_TYPE_FIELD_REQUIRED],
+            "items.ad"          => [$validator::ERROR_TYPE_FIELD_REQUIRED],
+        ], $validator->getReport()['/vod/feed']);
+    }
+
+    public function testVodFeedFieldsType()
+    {
+        // mock response
+        $this->_clientAPI->addSubscriber(new \Guzzle\Plugin\Mock\MockPlugin(array(
+            // auth
+            new \Guzzle\Http\Message\Response(200, array(
+                'Content-type' => 'application/json',
+            ), json_encode($this->_demoUser)),
+
+            // wrong types globals
+            new \Guzzle\Http\Message\Response(200, array(
+                'Content-type' => 'application/json',
+            ), json_encode([
+                    "error" => 0,
+                    "total_items" => "",
+                    "items" => ""
+            ])),
+
+            // wrong types in item dictionary
+            new \Guzzle\Http\Message\Response(200, array(
+                'Content-type' => 'application/json',
+            ), json_encode([
+                "error" => 0,
+                "total_items" => 200,
+                "items" => [
+                    [
+                        "id" => "id",
+                        "name" => 1,
+                        "description" => 1,
+                        "hd" => "hd",
+                        "year" => "year",
+                        "rate" => "rate",
+                        "category" => "cat",
+                        "genres" => "",
+                        "director" => 1,
+                        "actors" => 1,
+                        "country" => 1,
+                        "duration" => "duration",
+                        "url" => 1,
+                        "torrent" => 1,
+                        "hlts1" => 1,
+                        "filesize" => "5Mb",
+                        "thumb" => 1,
+                        "ad" => 1
+                    ], [
+                        "id" => "1",
+                        "name" => "Из Парижа с любовью",
+                        "description" => "Примерного сотрудника американского посольства...",
+                        "hd" => "2",
+                        "year" => "2010",
+                        "rate" => "6",
+                        "category" => "1",
+                        "director" => "Пьер Морел",
+                        "actors" => "Джон Траволта, Джонатан Рис-Майерс, Касия Смутняк...",
+                        "duration" => "92",
+                        "url" => "http://ivf.goweb.com/apt/ba.mp4?sign=yQqhDoGyXybw-hIdPgtchg&exp=1427454471",
+                        "torrent" => "http://server.com/path-to.torrent",
+                        "filesize" => 6584893674,
+                        "thumb" => "http://iptv.dev.telehouse-ua.net/screenshots/000/003/380.jpg",
+                        "ad" => []
+                    ],
+                ]
+            ])),
+        )));
+
+        $validator = new ValidatorWrapper($this->_clientAPI);
+
+        // wrong types globals
+        $validator->checkVodFeed();
+
+        // wrong types in item dictionary
+        $validator->checkVodFeed();
+
+        $this->assertEquals([
+            "total_items"       => [$validator::ERROR_TYPE_FIELD_MUSTBEINT],
+            "items"             => [$validator::ERROR_TYPE_FIELD_MUSTBEARRAY],
+            "items.id"          => [$validator::ERROR_TYPE_FIELD_MUSTBEINT],
+            "items.name"        => [$validator::ERROR_TYPE_FIELD_MUSTBESTRING],
+            "items.description" => [$validator::ERROR_TYPE_FIELD_MUSTBESTRING],
+            "items.hd"          => [$validator::ERROR_TYPE_FIELD_MUSTBEINT, $validator::ERROR_TYPE_FIELD_OUTOFRANGE],
+            "items.year"        => [$validator::ERROR_TYPE_FIELD_MUSTBEINT],
+            "items.rate"        => [$validator::ERROR_TYPE_FIELD_MUSTBEINT, $validator::ERROR_TYPE_FIELD_OUTOFRANGE],
+            "items.category"    => [$validator::ERROR_TYPE_FIELD_MUSTBEINT],
+            "items.genres"      => [$validator::ERROR_TYPE_FIELD_MUSTBEARRAY],
+            "items.director"    => [$validator::ERROR_TYPE_FIELD_MUSTBESTRING],
+            "items.actors"      => [$validator::ERROR_TYPE_FIELD_MUSTBESTRING],
+            "items.country"     => [$validator::ERROR_TYPE_FIELD_MUSTBESTRING],
+            "items.duration"    => [$validator::ERROR_TYPE_FIELD_MUSTBEINT],
+            "items.url"         => [$validator::ERROR_TYPE_FIELD_MUSTBESTRING],
+            "items.torrent"     => [$validator::ERROR_TYPE_FIELD_MUSTBESTRING],
+            "items.hlts1"       => [$validator::ERROR_TYPE_FIELD_MUSTBESTRING],
+            "items.filesize"    => [$validator::ERROR_TYPE_FIELD_MUSTBEINT],
+            "items.thumb"       => [$validator::ERROR_TYPE_FIELD_MUSTBESTRING],
+            "items.ad"          => [$validator::ERROR_TYPE_FIELD_MUSTBESTRING, $validator::ERROR_TYPE_FIELD_MUSTBEARRAY],
+        ], $validator->getReport()['/vod/feed']);
     }
 }
